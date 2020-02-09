772fc0e033d2891118fb2e00d2cdd13c735a94b7
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-09 14:30:11 +0200

    Update stockanalysis.py

--- stockanalysis.py
+++ stockanalysis.py
@@ -1,20 +1,29 @@
 from tkinter import *
 from tkinter import messagebox as msg
 from tkinter import filedialog
+
+import pandas as pd 
+
 class Stock_Analysis ():
     def __init__(self,master):
         self.master = master
         self.master.title("Stock Analysis")
         self.master.geometry("250x120")
         self.master.resizable(False,False)
+        self.filename = ""
         
         self.menu = Menu(self.master)
         
         self.file_menu = Menu(self.menu,tearoff = 0)
         self.file_menu.add_command(label = "Insert a csv",command = self.insert_csv)
+        self.file_menu.add_command(label  = "Close csv",command = self.closef)
         self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
         self.menu.add_cascade(label = "File",menu=self.file_menu)
-        
+
+        self.show_menu = Menu (self.menu,tearoff = 0)
+        self.show_menu.add_command(label = "Show csv",command = self.showcsv)
+        self.menu.add_cascade(label = "Show", menu = self.show_menu)
+
         self.about_menu = Menu(self.menu,tearoff = 0)
         self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
         self.menu.add_cascade(label="About",menu=self.about_menu)
@@ -28,6 +37,15 @@ class Stock_Analysis ():
         self.master.bind('<Control-F1>',lambda event: self.helpmenu())
         self.master.bind('<Control-i>',lambda event:self.aboutmenu())
     
+    def showcsv(self):
+        df = pd.read_csv(self.filename)
+        msg.showinfo("DATA FRAME" , str(df))
+
+    
+    def closef(self):
+        pass
+
+
     def insert_csv(self):
         self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
                                                    filetypes=(("csv files","*.csv"),("all files","*.*")))
