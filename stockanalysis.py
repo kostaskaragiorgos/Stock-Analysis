f4fd8df3ace1d3f6e9d3490ed5dcf10ba5c91caa
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-11 22:57:02 +0200

    Update stockanalysis.py

--- stockanalysis.py
+++ stockanalysis.py
@@ -17,17 +17,17 @@ class Stock_Analysis ():
         self.menu = Menu(self.master)
         
         self.file_menu = Menu(self.menu,tearoff = 0)
-        self.file_menu.add_command(label = "Insert a csv",command = self.insert_csv)
+        self.file_menu.add_command(label = "Insert a csv",accelerator = 'Ctrl + O',command = self.insert_csv)
         self.file_menu.add_command(label  = "Close csv",command = self.closef)
         self.file_menu.add_command(label="Exit",accelerator= 'Alt+F4',command = self.exitmenu)
         self.menu.add_cascade(label = "File",menu=self.file_menu)
 
         self.show_menu = Menu (self.menu,tearoff = 0)
-        self.show_menu.add_command(label = "Show csv",command = self.showcsv)
+        self.show_menu.add_command(label = "Show csv",accelerator = 'Alt+S',command = self.showcsv)
         self.menu.add_cascade(label = "Show", menu = self.show_menu)
 
         self.range_menu = Menu (self.menu,tearoff = 0)
-        self.range_menu.add_command(label = "Show Date Range", command = self.daterange)
+        self.range_menu.add_command(label = "Show Date Range",accelerator = 'Ctrl + D',command = self.daterange)
         self.menu.add_cascade(label = "Range", menu = self.range_menu)
 
         self.about_menu = Menu(self.menu,tearoff = 0)
@@ -67,20 +67,22 @@ class Stock_Analysis ():
 
 
     def insert_csv(self):
-        self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
-                                                   filetypes=(("csv files","*.csv"),("all files","*.*")))
-        
         # csv file stracture : Date,Open,High,Low,Close,Adj Close,Volume
-        if ".csv" in self.filename:
-            self.df = pd.read_csv(self.filename)
-            if all([item in self.df.columns for item in ['Date','Open','High','Low','Close','Adj Close','Volume']]) == TRUE:
-                msg.showinfo("SUCCESS","CSV FILE ADDED SUCCESSFULLY")
+        
+        if self.filename == "":
+            self.filename = filedialog.askopenfilename(initialdir="/",title="Select csv file",
+                                                   filetypes=(("csv files","*.csv"),("all files","*.*")))
+            if ".csv" in self.filename:
+                self.df = pd.read_csv(self.filename)
+                if all([item in self.df.columns for item in ['Date','Open','High','Low','Close','Adj Close','Volume']]) == True:
+                    msg.showinfo("SUCCESS","CSV FILE ADDED SUCCESSFULLY")
+                else:
+                    self.filename = ""
+                    msg.showerror("ERROR" ,"NO PROPER CSV ") 
             else:
-                self.filename = ""
-                msg.showerror("ERROR" ,"NO PROPER CSV ") 
+                msg.showerror("ERROR","NO CSV IMPORTED")
         else:
-            msg.showerror("ERROR","NO CSV IMPORTED")
-
+            msg.showerror("Error" , " A CSV FILE IS ALREADY OPEN")
 
     def exitmenu(self):
         if msg.askokcancel("Quit?", "Really quit?"):
