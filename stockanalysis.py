ad647a2031516362076e5d55c31cdf862fb50094
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-02-23 15:35:04 +0200

    Update stockanalysis.py

--- stockanalysis.py
+++ stockanalysis.py
@@ -11,41 +11,51 @@ class StockAnalysis():
         self.filename = ""
         #menu
         self.menu = Menu(self.master)
-        
         self.file_menu = Menu(self.menu, tearoff=0)
-        self.file_menu.add_command(label="Insert a csv", accelerator='Ctrl + O', command=self.insert_csv)
-        self.file_menu.add_command(label="Close csv", command=self.closef)
+        self.file_menu.add_command(label="Insert a csv",
+                                   accelerator='Ctrl + O', command=self.insert_csv)
+        self.file_menu.add_command(label="Close csv",
+                                   command=self.closef)
         self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
         self.menu.add_cascade(label="File", menu=self.file_menu)
-
         self.show_menu = Menu(self.menu, tearoff=0)
-        self.show_menu.add_command(label="Show csv", accelerator='Alt+S', command=self.showcsv)
-        self.show_menu.add_command(label="Show Open Graph", accelerator='Alt + T', command=self.opengraph)
-        self.show_menu.add_command(label="Show High Graph", accelerator='Alt + U', command=self.highgraph)
-        self.show_menu.add_command(label="Show Low Graph", accelerator='Alt + Q', command=self.lowgraph)
-        self.show_menu.add_command(label="Show Close Graph", accelerator='Alt + C', command=self.closegraph)
-        self.show_menu.add_command(label="Show Adj Close Graph", accelerator='Alt + D', command=self.adjclosegraph)
-        self.show_menu.add_command(label="Show Volume Graph", accelerator='Alt + B', command=self.volumegraph)
+        self.show_menu.add_command(label="Show csv",
+                                   accelerator='Alt+S', command=self.showcsv)
+        self.show_menu.add_command(label="Show Open Graph",
+                                   accelerator='Alt + T', command=self.opengraph)
+        self.show_menu.add_command(label="Show High Graph",
+                                   accelerator='Alt + U', command=self.highgraph)
+        self.show_menu.add_command(label="Show Low Graph",
+                                   accelerator='Alt + Q', command=self.lowgraph)
+        self.show_menu.add_command(label="Show Close Graph",
+                                   accelerator='Alt + C', command=self.closegraph)
+        self.show_menu.add_command(label="Show Adj Close Graph", 
+                                   accelerator='Alt + D', command=self.adjclosegraph)
+        self.show_menu.add_command(label="Show Volume Graph",
+                                   accelerator='Alt + B', command=self.volumegraph)
         self.menu.add_cascade(label="Show", menu=self.show_menu)
-
         self.range_menu = Menu(self.menu, tearoff=0)
-        self.range_menu.add_command(label="Show Date Range", accelerator='Ctrl + D', command=self.daterange)
-        self.range_menu.add_command(label="Show Open Range", accelerator='Alt + O', command=self.openrange)
-        self.range_menu.add_command(label="Show High Range", accelerator='Alt + H', command=self.highrange)
-        self.range_menu.add_command(label="Show Low Range", accelerator='Alt + L', command=self.lowrange)
-        self.range_menu.add_command(label="Show Close Range", accelerator='Alt + C', command=self.closerange)
-        self.range_menu.add_command(label="Show Adj Close Range", accelerator='Alt + A', command=self.adjcloserange)
+        self.range_menu.add_command(label="Show Date Range",
+                                    accelerator='Ctrl + D', command=self.daterange)
+        self.range_menu.add_command(label="Show Open Range", 
+                                    accelerator='Alt + O', command=self.openrange)
+        self.range_menu.add_command(label="Show High Range",
+                                    accelerator='Alt + H', command=self.highrange)
+        self.range_menu.add_command(label="Show Low Range",
+                                    accelerator='Alt + L', command=self.lowrange)
+        self.range_menu.add_command(label="Show Close Range", 
+                                    accelerator='Alt + C', command=self.closerange)
+        self.range_menu.add_command(label="Show Adj Close Range", 
+                                    accelerator='Alt + A', 
+                                    command=self.adjcloserange)
         self.range_menu.add_command(label="Show Volume Range", accelerator='Alt + V', command=self.volumerange)
         self.menu.add_cascade(label="Range", menu=self.range_menu)
-
         self.about_menu = Menu(self.menu, tearoff=0)
         self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
         self.menu.add_cascade(label="About", menu=self.about_menu)
-        
         self.help_menu = Menu(self.menu, tearoff=0)
         self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
-        
         self.master.config(menu=self.menu)
         self.master.bind('<Control-o>', lambda event: self.insert_csv())
         self.master.bind('<Alt-s>', lambda event: self.showcsv())
@@ -73,7 +83,7 @@ class StockAnalysis():
     def highgraph(self):
         """ shows high graph """
         if self.filename == "":
-            msg.showinfo("ERROR", "NO CSV FILE")    
+            msg.showinfo("ERROR", "NO CSV FILE")
     def lowgraph(self):
         """ shows low graph"""
         if self.filename == "":
@@ -91,7 +101,7 @@ class StockAnalysis():
         if self.filename == "":
             msg.showinfo("ERROR", "NO CSV FILE")
     def showcsv(self):
-        """ shows the whole dataset """ 
+        """ shows the whole dataset """
         if self.filename == "":
             msg.showinfo("ERROR", "NO CSV FILE")
         else:
@@ -103,7 +113,7 @@ class StockAnalysis():
         else:
             from1 = self.df.iloc[0]['Date']
             to1 = self.df.iloc[-1]['Date']
-            msg.showinfo("Date Range", "From: "+str(from1) +"\nTo: " +str(to1))    
+            msg.showinfo("Date Range", "From: "+str(from1) +"\nTo: " +str(to1))
     def openrange(self):
         """ shows the range of Open """
         if self.filename == "":
@@ -112,7 +122,6 @@ class StockAnalysis():
             max = self.df['Open'].max()
             min = self.df['Open'].min()
             msg.showinfo("Open Range", "Max: " +str(max) +"\nMin: " +str(min))
-        
     def highrange(self):
         """ shows the range of High """
         if self.filename == "":
@@ -121,7 +130,6 @@ class StockAnalysis():
             max = self.df['High'].max()
             min = self.df['High'].min()
             msg.showinfo("High Range", "Max: "+str(max) + "\nMin: " +str(min))
-    
     def lowrange(self):
         """ shows the range of Low """
         if self.filename == "":
@@ -130,7 +138,6 @@ class StockAnalysis():
             max = self.df['Low'].max()
             min = self.df['Low'].min()
             msg.showinfo("Low Range", "Max: "+str(max) + "\nMin: " +str(min))
-    
     def closerange(self):
         """ shows the range of Close """
         if self.filename == "":
@@ -157,10 +164,8 @@ class StockAnalysis():
             max = self.df['Volume'].max()
             min = self.df['Volume'].min()
             msg.showinfo("Volume Range", "Max: "+str(max) + "\nMin: " +str(min))
-    
-        
     def closef(self):
-        """ closes file """ 
+        """ closes file """
         if self.filename == "":
             msg.showinfo("ERROR", "NO CSV FILE")
         else:
@@ -169,17 +174,18 @@ class StockAnalysis():
 
     def insert_csv(self):
         """ insert csv function """
-        # csv file stracture : Date,Open,High,Low,Close,Adj Close,Volume        
+        # csv file stracture : Date,Open,High,Low,
+        # Close,Adj Close,Volume
         if self.filename == "":
-            self.filename = filedialog.askopenfilename(initialdir="/", title="Select csv file", 
-                                                    filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+            self.filename = filedialog.askopenfilename(initialdir="/", title="Select csv file",
+                                                       filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
             if ".csv" in self.filename:
                 self.df = pd.read_csv(self.filename)
-                if all([item in self.df.columns for item in ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]) == True:
+                if all([item in self.df.columns for item in ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]):
                     msg.showinfo("SUCCESS", "CSV FILE ADDED SUCCESSFULLY")
                 else:
                     self.filename = ""
-                    msg.showerror("ERROR", "NO PROPER CSV ") 
+                    msg.showerror("ERROR", "NO PROPER CSV ")
             else:
                 msg.showerror("ERROR", "NO CSV IMPORTED")
         else:
@@ -198,7 +204,7 @@ class StockAnalysis():
 def main():
     """ main function"""
     root = Tk()
-    Stock_Analysis(root)
+    StockAnalysis(root)
     root.mainloop()
     
 if __name__ == '__main__':
